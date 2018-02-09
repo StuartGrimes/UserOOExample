c18b94db6626d4b8a54cf9a4e6b5b7b08ef9be78
StuartGrimes <17004808+StuartGrimes@users.noreply.github.com>
2018-02-09 20:39:52 +0000

    simple app walk through to see object creation pattern

--- src/main/java/StuartGrimes/App.java
+++ src/main/java/StuartGrimes/App.java
@@ -1,13 +1,38 @@
 package StuartGrimes;
 
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private Scanner someInput;
+    private Date today;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App anApp = new App();
     }
+
+    public App(){
+
+        this.someInput = new Scanner(System.in);
+
+        sayHi();
+
+        System.out.println("\n Press enter to exit the program.");
+        this.someInput.nextLine();
+
+        System.exit(0);
+    }
+
+    private void sayHi(){
+        this.today = new Date();
+        System.out.println("App says hi at " + today);
+    }
+
+
 }
